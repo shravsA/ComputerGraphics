e9e3cd84f11f360bdbc03ad674da4909184f4957
systnock <64315409+systnock@users.noreply.github.com>
2020-09-12 12:16:33 +0530

    Header

---
+++ first.h
@@ -0,0 +1,10 @@
+//Header file
+#ifndef _first_page_h
+#define _first_page_h
+#include<GL/glut.h>
+#include<string.h>
+#include<stdlib.h>
+#include<stdio.h>
+extern int spotmain();
+extern void Start1();
+#endif // _first_page_h
